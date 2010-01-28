5e7c06a88e452f1875a11c1229cceeacc772dc41
Roman Smirnov <roman@railsdog.com>
2010-01-28 15:00:39 +0300

    Added validations to ShippingRestriction

--- app/models/shipping_restriction.rb
+++ app/models/shipping_restriction.rb
@@ -2,6 +2,9 @@ class ShippingRestriction < ActiveRecord::Base
   belongs_to :product
   belongs_to :product_group
   belongs_to :zone
+  validates_presence_of :zone
+  validates_presence_of :product, :if => Proc.new { |sr| sr.product_group.nil? }
+  validates_presence_of :product_group, :if => Proc.new { |sr| sr.product.nil? }  
 
   def covers?(product)
     product == self.product || product.product_groups.include?(self.product_group)
