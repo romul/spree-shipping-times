68efa6d106b6016d54949b734330c1532b065689
Roman Smirnov <roman@railsdog.com>
2010-01-28 15:02:04 +0300

    Added admin links for shipment centers and shipping restrictions to configuration page.

--- shipping_times_hooks.rb
+++ shipping_times_hooks.rb
@@ -1,5 +1,7 @@
 class ShippingTimesHooks < Spree::ThemeSupport::HookListener
 
   insert_after :product_description, "products/shipping_restrictions"
+  insert_after :admin_configurations_menu, "admin/shipment_centers/admin_link"
+  insert_after :admin_configurations_menu, "admin/shipping_restrictions/admin_link"
 
 end
